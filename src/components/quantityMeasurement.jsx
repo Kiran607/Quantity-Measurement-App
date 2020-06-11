363c7b3047c1d76386ca9ec67e2560cb471865a1
Kiran607 <ickirankiru@gmail.com>
2020-06-11 14:27:26 +0530

    [Kiran]Refactored UI

--- src/components/quantityMeasurement.jsx
+++ src/components/quantityMeasurement.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
-import '../services/quantityMeasurement.css';
+import '../App.css';
+import LengthMeasurement from '../components/LengthMeasurement';
+import TemperatureMeasurement from '../components/TemperatureMeasurement';
+import VolumeMeasurement from '../components/VolumeMeasurement';
+import { Container, Row, Col } from 'reactstrap';
 
-class QuantityMeasurement extends React.Component{
-    render(){
-        return(
+class QuantityMeasurement extends React.Component {
+    render() {
+        return (
             <div className="quantityForm">
-                <label htmlFor="text" id="text"><h2>Quanment</h2></label>
-                <div className="rectangle">
-                <h1>Welcome to Quantity Measurement</h1>
+                <div className="header">
+                    <header>
+                        <h1>Welcome to quantity Measurement</h1>
+                    </header>
+                </div>
+                <div className="text">CHOOSE TYPE</div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <LengthMeasurement />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <TemperatureMeasurement />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <VolumeMeasurement />
+                        </Col>
+                    </Row>
+                </Container>
+                <div className="measurement">
                 </div>
             </div>
         );
     }
-} 
+}
 export default QuantityMeasurement;
