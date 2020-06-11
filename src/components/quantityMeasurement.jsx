ff375ad42e78acdc80311d94b90c73ec3bf02c09
Kiran607 <ickirankiru@gmail.com>
2020-06-11 19:00:15 +0530

    [Kiran]Refactored react page with converter form

--- src/components/quantityMeasurement.jsx
+++ src/components/quantityMeasurement.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../App.css';
-import LengthMeasurement from '../components/LengthMeasurement';
-import TemperatureMeasurement from '../components/TemperatureMeasurement';
-import VolumeMeasurement from '../components/VolumeMeasurement';
+import LengthMeasurement from './LengthMeasurement';
+import TemperatureMeasurement from './TemperatureMeasurement';
+import VolumeMeasurement from './VolumeMeasurement';
 import { Container, Row, Col } from 'reactstrap';
 
 class QuantityMeasurement extends React.Component {
@@ -32,7 +32,32 @@ class QuantityMeasurement extends React.Component {
                         </Col>
                     </Row>
                 </Container>
-                <div className="measurement">
+                <div className="converterBody">
+                    <h4 className="converter-title">From To</h4>
+                    <input type="text" value="1" id="input"/>
+                    <span> </span>
+                    <input type="text" value="100" id="result"/><br/>
+                    <select id="inputType">
+                        <option value="meter">Meteres</option>
+                        <option value="kilometer">Kilometere</option>
+                        <option value="centimeters">Centimeteres</option>
+                        <option value="millimeter">Millimetere</option>
+                        <option value="micrometer">Micrometere</option>
+                        <option value="mile">Mile</option>
+                        <option value="foot">Foot</option>
+                        <option value="inch">Inch</option>
+                    </select>
+                    <span> </span>
+                    <select id="resultType">
+                        <option value="meter">Meteres</option>
+                        <option value="kilometer">Kilometere</option>
+                        <option value="centimeters">Centimeteres</option>
+                        <option value="millimeter">Millimetere</option>
+                        <option value="micrometer">Micrometere</option>
+                        <option value="mile">Mile</option>
+                        <option value="foot">Foot</option>
+                        <option value="inch">Inch</option>
+                    </select>
                 </div>
             </div>
         );
