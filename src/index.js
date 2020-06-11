bd62e3d84ef8151529b252de9918a64eb0a1b952
Kiran607 <ickirankiru@gmail.com>
2020-06-11 10:33:58 +0530

    [Kiran]Refactored ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import QuantityMeasurement from './components/quantityMeasurement';
-import '../src/services/quantityMeasurement.css';
+import QuantityMeasurement from './components/QuantityMeasurement';
+import '../src/App.css';
 import * as serviceWorker from './serviceWorker';
+// import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
