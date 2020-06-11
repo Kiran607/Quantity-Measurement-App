2a7966aaf9d7a30312ce3b2e0a2916284166143c
Kiran607 <ickirankiru@gmail.com>
2020-06-11 20:21:11 +0530

    [Kiran]Added  index js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import QuantityMeasurement from './components/QuantityMeasurement';
 import '../src/App.css';
 import * as serviceWorker from './serviceWorker';
-// import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
