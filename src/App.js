b4f08f94ade8c59fe88630532e1714edaf75e486
Kiran607 <ickirankiru@gmail.com>
2020-06-17 12:59:00 +0530

    [Kiran]Added router file for project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import QuantityMeasurement from './components/QuantityMeasurement';
+import VolumeQuantity from './components/VolumeQuantity';
+import TemperatureQuantity from './components/TemperatureQuantity';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/lengthQuantity" exact component={QuantityMeasurement} />
+      <Route path="/volumeQuantity" exact component={VolumeQuantity}/>
+      <Route path="/temperatureQuantity" exact component={TemperatureQuantity} />
+      </Switch>
+    </Router>
   );
 }
 
